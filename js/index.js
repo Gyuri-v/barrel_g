3ce382d96b000ccbab3dd435ecf0f62aa2b747d0
gyuri <fnffkdll@naver.com>
2020-07-15 05:23:58 +0900

    main_cat & main_brand position event 1차

--- js/index.js
+++ js/index.js
@@ -94,13 +94,52 @@ $(document).ready(function(){
       },
     })
     
+    //category
+    //                                               이거 각각 하는법 물어보기
+//    $('.bg').scroll(function() {
+//        var x = $(this).scrollTop();
+//        $(this).css('background-position', '0% ' + parseInt(-x / 10) + 'px');
+//    });
+    $(window).scroll(function(){
+        if($(document).scrollTop()*1.4 > $('.cat01').offset().top){
+            var x = $(this).scrollTop();
+            $('.cat01').css('background-position', '0% ' + parseInt(-x / 10 + 150)  + 'px');
+        }
+        if($(document).scrollTop()*1.4 > $('.cat02').offset().top){
+            var x = $(this).scrollTop();
+            $('.cat02').css('background-position', '0% ' + parseInt(-x / 10 + 180)  + 'px');
+        }
+        if($(document).scrollTop()*1.4 > $('.cat03').offset().top){
+            var x = $(this).scrollTop();
+            $('.cat03').css('background-position', '0% ' + parseInt(-x / 10 + 210)  + 'px');
+        }
+        if($(document).scrollTop()*1.4 > $('.cat04').offset().top){
+            var x = $(this).scrollTop();
+            $('.cat04').css('background-position', '0% ' + parseInt(-x / 10 + 240)  + 'px');
+        }
+        if($(document).scrollTop()*1.4 > $('.cat05').offset().top){
+            var x = $(this).scrollTop();
+            $('.cat05').css('background-position', '0% ' + parseInt(-x / 10 + 270)  + 'px');
+        }
+        if($(document).scrollTop()*1.4 > $('.cat06').offset().top){
+            var x = $(this).scrollTop();
+            $('.cat06').css('background-position', '0% ' + parseInt(-x / 10 + 200)  + 'px');
+        }
+    })
     
     //brand txt moving
-//	var storyOffset = $('.brand_story').offset();
+//	  var storyOffset = $('.brand_story').offset();
 //    $(window).scroll(function(){
 //        if( $(document).scrollTop() > storyOffset.top - 500 ){
 //            $('.brand_story h3').animate({left : '25px'},700);
 //        }
 //    })
+    $(window).scroll(function(){
+        if($(document).scrollTop()*1.4 > $('.brand_story').offset().top){
+            var x = $(this).scrollTop();
+            $('.brand_story').find('h3').css('left', parseInt( x / 10 - 350)  + 'px');
+            $('.brand_visual').find('h3').css('right', parseInt( x / 10 - 440)  + 'px');
+        }
+    })
 
 })
